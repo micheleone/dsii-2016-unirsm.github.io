01199cdb813463be0daade71761833785c4267bc
CaterinaLipari <liparicaterina92@gmail.com>
2016-05-18 15:40:53 +0200

    Update sketch.js

--- p5/10print/caterina/sketch.js
+++ p5/10print/caterina/sketch.js
@@ -7,60 +7,67 @@ var x = 0;  // store current x position on the grid
 var y = 0;  // store current y position on the grid
 var g = 10; // grid size
 var colore = 255; // current grid color
-
+var ciclo=1;
 
 function setup() {
    pixelDensity(displayDensity());
    createCanvas(windowWidth, windowHeight);
-   //
-   background(200);
-   //
    url = getURL();
    //strokeJoin(ROUND);
    cursor(HAND);
+   ellipseMode(CENTER);
  }
 
 function draw() {
-  preparaQuadretto();
-  // riga proporzionale alla griglia
-  strokeWeight(g/random(5));
-  // lancio la monetina e disegno la riga corrispondente
-  if (testa()) {
-  line(x*g,y,x*g,height); //linee dritte
-  //line(x*g,height-g*y,x*g+g,height/colore-g*y-g); //linee storte
-  } else {
-    //(x*g,height-g*y-g,x*g+g,height-g*y);
-  }
-  // passo alla casella a lato
-  x++;
-  // se sono in fondo alla riga vado a capo alla riga successiva
-  if (x*g >= width) {
-    x=0;
-//se le righe arrivano in fondo ricomincia dall'inizio e lancia la moneta
-    colore = random(255); 
-    g = 5+random(width);
-    background(colore-255);
-    preparaQuadretto();
+  //background(colore-255); solo una linea che avanza
+    strokeWeight(g/random(5));
+    stroke(colore);
 
-  //  y++;
-    preparaQuadretto();
-  }
-  // se sono in fondo alla pagina ricomincio con colore e griglia differente
-  /*if (y*g >= height) {
-    y=0;
-    colore = random(255);
-    g = 5+random(width);
-    preparaQuadretto();
-  }*/
+  // lancio la monetina e disegno 
+  if (testa()) {
+ 
+if (ciclo==1){
+  //variante linee
+      line(x*g,y,x*g,height); //linee dritte
+ //variante linee
 }
+if (ciclo==2){
+   //variante cerchio
+      ellipse(x*g,height/2,x+g,x+g); //cerchio al centro 
+ //variante cerchio
+}
+if (ciclo==3){
+
+    //variante cerchio Random
+      ellipse(x*g,height-random(height),x+g,x+g); //cerchio al centro 
+ //variante cerchio Random
+}
+      // passo alla casella a lato
+  x++;
+
+  }  // lancio la monetina e disegno 
+
+
+  // se sono in fondo alla riga vado a capo, cambio colore e cambia g
+  if (x*g >= width) {
+    ciclo++;
+    x=0;
+    colore = random(255); 
+    g = 5+random(20);
+    background(colore-255);
+  }
+  if (ciclo>= 4) {
+    ciclo=1;
+  } 
+  } //chiudi draw
 
 // riparti se premi il mouse
 function mousePressed() {
+  ciclo++;
   x=0;
   y=0;
   colore = random(255);
-  g = 10+random(width);
-  preparaQuadretto();
+  g = 10+random(20);
 }
 
 // lancio della monetina
@@ -72,22 +79,6 @@ function testa() {
   }
 }
 
-// cancella una riga prima di disegnarci
-function preparaRiga() {
-  /*fill(colore);
-  noStroke();
-  rect(0,height-g*y-g-1,width,g+1);
-  stroke(255-colore);*/
-}
-
-// cancella un quadretto prima di disegnarci
-function preparaQuadretto() {
-  /*fill(colore);*/
-  noStroke();
-  //rect(x*g,height-g*(y+1),g-1,g-1);
-  //rect(x*g,height-g*(y+1),g-(g/20),g-(g/20));
-  stroke(255-colore);
-}
 
 // se ridimensiona la finestra ricalcola width e height canvas
 function windowResized() {
