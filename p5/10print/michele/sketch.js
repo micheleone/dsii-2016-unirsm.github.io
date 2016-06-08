5a46a46a5a29227470996d4b4233970e708b142c
micheleone <michele.leone20@gmail.com>
2016-06-08 18:05:40 +0200

    Update sketch.js

--- p5/10print/michele/sketch.js
+++ p5/10print/michele/sketch.js
@@ -1,52 +1,102 @@
-var theta;
+var cells; // griglia delle celle
+var stack; // stack viene usato per salvare le celle visitate
+var current;  // cella corrente
+var maze; // variabile per disegnare il labirinto
+
 
 function setup() {
- createCanvas(640, 420);
- background(0,130,200);
+  createCanvas(windowWidth, windowHeight);
+  background(0, 130, 200);
+  
+  initializeVariables();
+  createGrid();
 }
 
-function draw(30) {
-	frameRate(); // Numero di frame
-	 strokeWeight(1); // Spessore delle linee
-	 var a = (mouseX / (var) width) * 360f; // Generazione di un angolo da 0 a 360 gradi in base alla posizione del mouse
-	 theta = radians(a); // Conversione in radianti
-	 translate(width/2, height); // Posizione delle linee sull'asse x
-	  translate(0, -170); // Posizione delle linee sull'asse y
-	  //slash(120); // Numero di ramificazioni
-}
-function slash(var h) {
-h *= 0.66; // Ogni linea è lunga 2/3 rispetto alle dimensioni di quella precedente
-		if (mouseIsPressed) {
-    background(0,130,200); // Cliccando con il mouse si resetta la schermata
-  } else {
-		if (h > 20) {
-			push();    // Salvataggio dello stato attuale della trasformazione (cioè il sistema di coordinate corrente)
-			rotate(theta);   // Rotazione delle linee
-			var c = map(mouseX, 0, width, 255, 0); // Controllo del movimento del primo gruppo attraverso il mouse
-			stroke(255, c, 0); // Colorazione della linea
-			line(-10, 0, 0, -h);  // Creazione della prima linea   
-            translate(0, -h); // Posizionamento sugli assi x e y della prima linea
-            line(-10, 0, 0, h); 
-            translate(h, 0);
-            slash(h); // Creazione di sottogruppi di linee
-            pop(); // Ripristino dello stato precedente della matrice
-// Ripetizione delle operazione per il secondo gruppo di linee
-            push();    // Salvataggio dello stato attuale della trasformazione (cioè il sistema di coordinate corrente)
-			rotate(-theta);   // Rotazione delle linee
-			var c = map(mouseX, 0, width, 0, 255); // Controllo del movimento del primo gruppo attraverso il mouse
-			stroke(255); // Colorazione della linea
-			line(10, 0, 0, -h);  // Creazione della prima linea   
-            translate(0, -h); // Posizionamento sugli assi x e y della prima linea
-            line(10, 0, 0, h); 
-            translate(-h, 0);
-            slash(h);       // Creazione di sottogruppi di linee
-            pop();
-        }
-    }
-    if (keyIsPressed) { // premendo il tasto "s", l'immagine "frame.jpg" viene salvata nello Sketch folder
-    	if (key == 's') {
-    		save("frame.jpg");
-    	}
-    }
+
+function draw() {
+  updateMaze();
+  displayMaze();
 }
 
+
+function mousePressed() {
+  createGrid();
+}
+
+
+function initializeVariables() {
+  maze = createGraphics(600, 300);
+  Cell.l = 22;
+  
+  Cell.h = Cell.l*sqrt(3)/2;
+  Cell.r = Cell.h*2/3;
+  Cell.theta = TWO_PI/3;
+  
+  Cell.cols = floor(1.8*maze.width/Cell.l)-3;
+  Cell.rows = floor(maze.height/Cell.h);
+}
+
+
+function createGrid() {
+  cells = [];
+  stack = [];
+  
+  var x = 0, y = 0, up = false;
+  
+  for (var j = 0; j < Cell.rows; j += 1) {
+    for (var i = j; i <= Cell.cols-j; i += 1) {
+      
+      if (i%2 === 0) {
+        up = j%2  === 0 ? false : true;
+      } else {
+        up = j%2  === 1 ? false : true;
+      }
+      
+      x = Cell.l + i*Cell.l/2;
+      y = up ? Cell.r : Cell.h - Cell.r; 
+      y += j*Cell.h;
+      
+      cells.push(new Cell(x, y, up));
+    }
+  }
+  
+  current = cells[0];
+}
+
+
+function updateMaze() {
+  maze.background(0, 130, 200);
+  
+  current.visited = true;
+  
+  // codice per passare al neighbor
+  var next = current.chooseNeighbor();
+  if (next) {
+    stack.push(current);  
+    Cell.removeWalls(current, next);
+    current = next;
+  } else if (stack.length > 0) {
+    current = stack.pop();
+  }
+  
+  // codice per visualizzare le celle
+  for (var i = 0, l = cells.length; i < l; i += 1) {
+    cells[i].display(maze);
+  }
+  
+  // codice per evidenziare la cella corrente
+  if (stack.length > 0) {
+    current.highlight(maze);
+  }
+}
+
+// posizione e dimensione di tutto il disegno
+function displayMaze() {
+  translate(width/2, (height - maze.width)/2);
+  rotate(HALF_PI);
+  
+  image(maze, -40, 0);
+  
+  scale(1, -1);
+  image(maze, 90, 0);
+}
